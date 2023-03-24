58c0506993f0ccd23a1f2addd20674b85e9c6759
Micheal Berkeley <54487168+mza1999@users.noreply.github.com>
2023-03-25 02:21:01 +0300

    added Transaction model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Transaction(models.Model):
+    amount = models.IntegerField()
+    serviceType = models.TextField()
+    orderid = models.TextField()
+    redirectUrl = models.TextField()
+    iat = models.IntegerField()
+    exp = models.IntegerField()
+    lang = models.TextField()
+    
+

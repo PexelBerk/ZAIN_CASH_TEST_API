6f052984c23950f14d897114596686699156ea88
Micheal Berkeley <54487168+mza1999@users.noreply.github.com>
2023-03-25 14:42:20 +0300

    otp confirmation and check

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("transaction/init", views.init_transaction, name="init_transaction"),
     path("transaction/pay", views.pay_transaction, name="pay_transaction"),
+    path("transaction/otp", views.otp_confirmation, name="otp_confirmation"),
+    path("transaction/otp/check", views.check_otp, name="check_otp"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
-
+from django.db import transaction as trans
 
 from .models import Merchant, Transaction, Account
 
 import json
 import jwt
+import time
 
 # Create your views here.
 
@@ -73,13 +74,127 @@ def pay_transaction(request):
     context = {
         "merchant":transaction.merchant,
         "amount":transaction.amount,
+        "operation_id":transaction.operation_id,
     }
 
 
     return render(request, "payment.html", context)
 
+@api_view(['POST'])
+@csrf_exempt
+def otp_confirmation(request):
+    if request.method == "POST":
+        phone = request.data['phone']
+        pin   = request.data['pin']
+        operation_id = request.data['operation_id']
+
+        
+        try:
+            transaction = Transaction.objects.get(operation_id=operation_id)
+        except:
+            return HttpResponse("no transaction with this id")
+
+        
+
+        try:
+            account = Account.objects.get(phone=phone)
+        except:
+            _time = int(time.time())
+            data = {
+                "status":"failed",
+                "msg":"wrong credentionals",
+                "orderid":transaction.orderId,
+                'iat': _time,
+                'exp': _time + 60 * 60 * 4
+            }
+            token = jwt.encode(data, transaction.merchant.secret, algorithm='HS256')
+            return redirect(transaction.redirectUrl+ f"?token={token}")
+
+        if account.pin != pin:
+            _time = int(time.time())
+            data = {
+                "status":"failed",
+                "msg":"wrong credentionals",
+                "orderid":transaction.orderId,
+                'iat': _time,
+                'exp': _time + 60 * 60 * 4
+            }
+            token = jwt.encode(data, transaction.merchant.secret, algorithm='HS256')
+            return redirect(transaction.redirectUrl+ f"?token={token}")
+        
+        if account.balance < transaction.amount:
+            _time = int(time.time())
+            data = {
+                "status":"failed",
+                "msg":"not enough balance",
+                "orderid":transaction.orderId,
+                'iat': _time,
+                'exp': _time + 60 * 60 * 4
+            }
+            token = jwt.encode(data, transaction.merchant.secret, algorithm='HS256')
+            return redirect(transaction.redirectUrl+ f"?token={token}")
+
+        transaction.account = account
+        transaction.save()
+        context = {
+            "operation_id":operation_id,
+            'amount':transaction.amount,
+            'balance':account.balance
+        }
+        return render(request, "otp.html", context)
+    
+    else:
+        return HttpResponse("most be post request")
 
 
+
+@api_view(['POST'])
+@csrf_exempt
+def check_otp(request):
+    if request.method == "POST":
+        operation_id = request.data['operation_id']
+        otp = request.data['otp']
+        
+        
+        try:
+            transaction = Transaction.objects.get(operation_id=operation_id)
+        except:
+            return HttpResponse("no transaction with this id")
+
+        if transaction.account.otp != otp:
+            _time = int(time.time())
+            data = {
+                "status":"failed",
+                "msg":"wrong creds",
+                "orderid":transaction.orderId,
+                'iat': _time,
+                'exp': _time + 60 * 60 * 4
+            }
+            token = jwt.encode(data, transaction.merchant.secret, algorithm='HS256')
+            return redirect(transaction.redirectUrl+ f"?token={token}")
+        
+
+        with trans.atomic():
+            transaction.account.balance -= transaction.amount
+            transaction.merchant.balance += transaction.amount
+            transaction.account.save()
+            transaction.merchant.save()
+
+        
+        _time = int(time.time())
+        data = {
+            "status":"success",
+            "msg":"success",
+            "orderid":transaction.orderId,
+            'iat': _time,
+            'exp': _time + 60 * 60 * 4
+        }
+        token = jwt.encode(data, transaction.merchant.secret, algorithm='HS256')
+        return redirect(transaction.redirectUrl+ f"?token={token}")
+
+
+    else:
+        return HttpResponse("most be post request")
 
 
 

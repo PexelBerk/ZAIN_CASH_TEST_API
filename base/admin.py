5b93db7ed6f7f849cca75ff1157199693bf59ef5
Micheal Berkeley <54487168+mza1999@users.noreply.github.com>
2023-03-25 03:49:25 +0300

    registered models to admin site

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Merchant, Transaction, Account
+
+admin.site.register(Merchant)
+admin.site.register(Transaction)
+admin.site.register(Account)
+
